7da48d0bf075d92eb0a9808ba487f5c2a2ad5bc0
Adam Walter <adam.iverson.walter@gmail.com>
2017-11-14 22:51:05 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -4,11 +4,13 @@ def badge_maker(name)
 end
 
 def batch_badge_creator(arr_of_names)
+  batch_arr = []
   arr_of_names.each do |name|
-    badge_maker(name)
+    batch_arr << badge_maker(name)
   end
+  return batch_arr
 end
 
 def assign_rooms
-  
+
 end
